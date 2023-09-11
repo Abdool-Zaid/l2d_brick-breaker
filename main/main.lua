0e7a0de15af59fda523728d2c795b492c86e6c1b
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-09-11 16:11:21 +0200

    first commit

---
+++ main/main.lua
@@ -0,0 +1,14 @@
+function love.load()
+    love.window.setTitle("brick breaker")
+ score= 0
+    love.window.setMode(1024, 768)
+
+end
+function love.update(dt)
+    coor.x, coor.y = love.mouse.getPosition()
+    
+end
+
+function love.draw()
+
+ end
